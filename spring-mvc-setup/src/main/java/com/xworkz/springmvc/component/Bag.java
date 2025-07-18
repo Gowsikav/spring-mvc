760952062b7b6a21c0be0c9bf73c0f2feefeded7
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-07-18 19:12:08 +0530

    complete task: Autowired and Qualifier

--- spring-mvc-setup/src/main/java/com/xworkz/springmvc/component/Bag.java
+++ spring-mvc-setup/src/main/java/com/xworkz/springmvc/component/Bag.java
@@ -1,9 +1,19 @@
 package com.xworkz.springmvc.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bag {
+
+    @Autowired
+    @Qualifier("snacks")
+    private Bakery bakery;
+
+    @Autowired
+    private Bakery snacks;
+
     public Bag()
     {
         System.out.println("Bag constructor");
