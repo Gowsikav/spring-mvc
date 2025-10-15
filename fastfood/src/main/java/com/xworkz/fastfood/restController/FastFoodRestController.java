3103b0ef6caa469b07b83761234424e5ed17b879
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-10-15 19:33:40 +0530

    feat: implement FastFood application with RESTful API and data handling

---
+++ fastfood/src/main/java/com/xworkz/fastfood/restController/FastFoodRestController.java
@@ -0,0 +1,67 @@
+package com.xworkz.fastfood.restController;
+
+import com.xworkz.fastfood.dto.FastFoodDTO;
+import com.xworkz.fastfood.service.FastFoodService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("api/fastFood")
+public class FastFoodRestController {
+
+    @Autowired
+    private FastFoodService fastFoodService;
+
+    public FastFoodRestController()
+    {
+        log.info("FastFoodRestController method in fastfood rest controller");
+    }
+
+    @PostMapping("/saveFoodData")
+    public ResponseEntity<String> saveFoodDetails(FastFoodDTO fastFoodDTO)
+    {
+        log.info("food details {}",fastFoodDTO);
+        if(fastFoodService.saveData(fastFoodDTO))
+        {
+            return ResponseEntity.ok("Data saved successfully");
+        }else {
+            return ResponseEntity.internalServerError().body("Not Saved");
+        }
+    }
+
+    @GetMapping("getFoodDataByName")
+    public ResponseEntity<FastFoodDTO> getDataByFoodName(@RequestParam String foodName)
+    {
+        log.info("getDataByFoodName method in FastFoodRestController");
+        FastFoodDTO dto=fastFoodService.getDataByFoodName(foodName);
+        if(dto==null)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(dto);
+
+    }
+
+    @PutMapping("updateFood")
+    public ResponseEntity<FastFoodDTO> updateFood(@RequestParam String foodName,
+                                                  @RequestBody FastFoodDTO updatedDto) {
+        log.info("updateFood method in FastFoodRestController");
+        FastFoodDTO dto = fastFoodService.updateFoodByName(foodName, updatedDto);
+        if (dto == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("deleteByFoodName")
+    public ResponseEntity<String> deleteByFoodName(@RequestParam String foodName)
+    {
+        log.info("deleteByFoodNme method in FastFoodRestController");
+        if(fastFoodService.deleteByFoodName(foodName))
+            return ResponseEntity.ok("Data Deleted");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Data not Deleted");
+    }
+}
---
+++ fastfood/src/main/java/com/xworkz/fastfood/service/FastFoodServiceImpl.java
@@ -0,0 +1,66 @@
+package com.xworkz.fastfood.service;
+
+import com.xworkz.fastfood.dto.FastFoodDTO;
+import com.xworkz.fastfood.entity.FastFoodEntity;
+import com.xworkz.fastfood.repository.FastFoodRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FastFoodServiceImpl implements FastFoodService{
+
+    @Autowired
+    private FastFoodRepository fastFoodRepository;
+
+    @Override
+    public boolean saveData(FastFoodDTO fastFoodDTO) {
+        log.info("save data method in fast food service");
+        FastFoodEntity fastFoodEntity=new FastFoodEntity();
+        BeanUtils.copyProperties(fastFoodDTO,fastFoodEntity);
+        fastFoodEntity=fastFoodRepository.save(fastFoodEntity);
+
+        return fastFoodEntity != null;
+    }
+
+    @Override
+    public FastFoodDTO getDataByFoodName(String foodName) {
+        log.info("getDataByFoodName method in fast food service");
+        FastFoodEntity fastFoodEntity=fastFoodRepository.findByFoodName(foodName);
+        if(fastFoodEntity!=null) {
+            FastFoodDTO dto=new FastFoodDTO();
+            BeanUtils.copyProperties(fastFoodEntity,dto);
+            return dto;
+        }
+        return null;
+    }
+
+    @Override
+    public FastFoodDTO updateFoodByName(String foodName, FastFoodDTO updatedDto) {
+
+        log.info("updateFoodByName method in fast food service");
+        FastFoodEntity fastFoodEntity=fastFoodRepository.findByFoodName(foodName);
+        if(fastFoodEntity!=null) {
+            updatedDto.setFoodId(fastFoodEntity.getFoodId());
+            BeanUtils.copyProperties(updatedDto,fastFoodEntity);
+            fastFoodEntity=fastFoodRepository.save(fastFoodEntity);
+            BeanUtils.copyProperties(fastFoodEntity,updatedDto);
+            return updatedDto;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteByFoodName(String foodName) {
+        log.info("deleteByFoodName method in fast food service");
+        FastFoodEntity fastFoodEntity=fastFoodRepository.findByFoodName(foodName);
+        if(fastFoodEntity!=null)
+        {
+            fastFoodRepository.delete(fastFoodEntity);
+            return true;
+        }
+        return false;
+    }
+}
